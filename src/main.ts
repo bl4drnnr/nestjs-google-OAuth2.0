adedd86cb399500b7439387baeea91e20c8fd2ca
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-09-21 17:10:48 +0200

    clean

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3000, () => {
+    console.log(`Server has been successfully started on port ${PORT}`);
+  });
 }
 bootstrap();
