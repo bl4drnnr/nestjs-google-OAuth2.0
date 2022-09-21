4c283f3dde1f28a31491b3de11b31e2a8d90cef0
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-09-21 18:05:18 +0200

    google auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { GoogleAuthGuard } from '../guards/google-auth.guard';
 
 @Controller('auth')
 export class AuthController {
+  @UseGuards(GoogleAuthGuard)
   @Get('google/login')
   login() {
     return '';
   }
 
+  @UseGuards(GoogleAuthGuard)
   @Get('google/redirect')
   redirect() {
     return '';
